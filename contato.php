e86f26e50d306e96d1b71be7f1ade65070c2c038
thiago-z <37704790+thiago-z@users.noreply.github.com>
2018-05-07 23:23:41 -0300

    Add pg contato

---
+++ contato.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html><!-- doctype do HTML5 -->
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html lang="pt-BR" class="no-js"><!-- A classe 'no-js' é reconhecida pelo javascript modernizr, permitindo que browsers antigos reconheçam corretamente nossas tags -->
+<!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        		
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"><!-- Importante para habilitar os recursos de Responsividade em conjunto com o CSS -->
+		
+		<title>Noleak - Contato </title>
+		
+		<script src="js/jquery-1.8.1.js"></script>
+        <script src="js/bootstrap.js"></script>
+		<script src="js/modernizr-2.6.1.min.js"></script>
+		
+		                
+		<link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link href="css/meu_estilo.css" rel="stylesheet" type="text/css">
+        <script defer src="js/fontawesome/fontawesome-all.js"></script>
+</head>
+<body>
+		
+        <!--[if lt IE 7]>
+            <p class="chromeframe">Você está usando um brower desatualizado. <a href="http://browsehappy.com/">Atualize seu browser</a> ou <a href="http://www.google.com/chromeframe/?redirect=true">instale Google Chrome Frame</a> para melhor visualizar este site.</p>
+        <![endif]-->
+
+        <!-- Código retidado de http://twitter.github.com/bootstrap/examples/hero.html -->
+		
+		<!-- Início da barra de navegação -->
+        
+        	<?php 
+			
+				require "barra_navegacao.php";
+	
+			?>
+        
+        
+		<!-- Fim da barra de navegação -->
+        <!-- Início do container para a classe 'hero-unit' -->
+		<div class="container-fluid hero">
+
+            <!-- Hero unit para o showcase -->
+            <div class="hero-unit">
+				<h1>Nossos profissionais trabalham pela sua segurança!</h1>
+				<br />
+                <p>A NOLEAK é uma empresa voltada para a segurança em sistemas de gases combustíveis e produtos perigosos.</p>
+
+				<p>Trabalhamos buscando a excelência em sistemas de monitoramento e de bloqueio do fluxo de gás em centrais de gás e em equipamentos consumidores.</p>
+
+				<p>Atendemos com nossos equipamentos padrão, bem como desenvolvendo projetos customizados.</p>
+				
+            </div>
+		</div> <!-- /container hero -->
+		
+		<div class="container-fluid submenu"><!-- Submenu -->
+			<div class="row-fluid">
+				<div class="span12">
+					<nav aria-label="breadcrumb">
+					  <ol class="breadcrumb">
+						<li class="breadcrumb-item"><a href="index.php">Home</a></li>
+						<li class="breadcrumb-item"><a href="produtos.php">Produtos</a></li>
+						<li class="breadcrumb-item"><a href="servicos.php">Serviços</a></li>
+						<li class="breadcrumb-item"><a href="artigos.php">Artigos</a></li>
+						<li class="breadcrumb-item"><a href="quem_somos.php">Quem somos</a></li>
+						<li class="breadcrumb-item active" aria-current="page">Contato</li>
+					  </ol>
+					</nav>
+				</div>
+			</div>
+		</div><!-- /Submenu -->
+		
+		
+		<!-- Aqui colocaremos nosso container e uma linha com 3 colunas -->
+		<div class="container-fluid">
+           <div class="container-fluid"><!-- Submenu -->
+			<div class="row-fluid">
+				<div class="span12">
+					<h3>Conheça os nossos técnicos</h3>
+				</div>
+			</div>
+		</div><!-- /Submenu -->
+            <div class="row-fluid">
+                <div class="span12">
+                   <div class="tecnico_container">
+                   		<img src="img/retrato.jpg" alt="">
+
+                   <article class="tecnico_descricao">
+
+						<h2>Nome do técnico</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dignissim risus non nisl scelerisque posuere. Nam pretium, velit id faucibus tempor, turpis mi tristique odio, sed interdum quam libero vitae magna. Aenean nisl ligula, condimentum sed iaculis nec, egestas vel ipsum. Donec vel neque ac sem laoreet ultrices.</p>
+						
+						<h4><i class="fas fa-graduation-cap"></i> Formação acadêmica</h4>
+							<li>Engenharia eletrnica</li>
+							<li>Engenharia eletrnica</li>
+                   
+                    </article>
+                </div>
+              </div>              
+           </div> <!-- /container 2--> 
+              
+           <div class="row-fluid">
+                <div class="span12">
+                   <div class="tecnico_container">
+                   		<img src="img/retrato.jpg" alt="">
+
+                   <article class="tecnico_descricao">
+
+						<h2>Nome do técnico</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dignissim risus non nisl scelerisque posuere. Nam pretium, velit id faucibus tempor, turpis mi tristique odio, sed interdum quam libero vitae magna. Aenean nisl ligula, condimentum sed iaculis nec, egestas vel ipsum. Donec vel neque ac sem laoreet ultrices.</p>
+						
+						<h4><i class="fas fa-graduation-cap"></i> Formação acadêmica</h4>
+							<li>Engenharia eletrnica</li>
+							<li>Engenharia eletrnica</li>
+                   
+                    </article>
+                </div>
+              </div>              
+           </div> <!-- /container 2-->    
+               
+            <hr>
+		<div class="container-fluid rodape">
+			<div class="row-fluid">
+				<div class="span3 footer">
+					<h4>Links</h4>
+					<ul>
+						<li><a href="" title="Ruby on Rails" rel="nofollow">Linha de Código</a></li>
+						<li><a href="" title="jQuery" rel="nofollow">jQuery</a></li>
+						<li><a href="" title="LESS CSS" rel="nofollow">LESS CSS</a></li>
+						<li><a href="" title="Wordpress" rel="nofollow">Wordpress</a></li>
+						<li><a href="" title="Joomla" rel="nofollow">Joomla</a></li>
+					</ul>
+				</div>
+				<div class="span3 footer">
+					<h4>Links</h4>
+					<ul>
+						<li><a href="" title="W3C" rel="nofollow">W3C</a></li>
+						<li><a href="" title="W3schools" rel="nofollow">W3Schools</a></li>
+						<li><a href="" title="PHP Oficial" rel="nofollow">PHP.net</a></li>
+						<li><a href="" title="MySQL" rel="nofollow">MySQL</a></li>
+						<li><a href="" title="Bootstrap" rel="nofollow">Bootstrap</a></li>
+					</ul>
+				</div>
+				<div class="span3 footer">
+					<h4>Links</h4>
+					<ul>
+						<li><a href="" title="" rel="nofollow">Link</a></li>
+						<li><a href="" title="" rel="nofollow">Link</a></li>
+						<li><a href="" title="" rel="nofollow">Link</a></li>
+						<li><a href="" title="" rel="nofollow">Link</a></li>
+						<li><a href="" title="" rel="nofollow">Link</a></li>
+					</ul>
+				</div>
+				<div class="span3 footer">
+					<h4>Citação da semana</h4>
+					<cite class="pull-right">"Dá-se muita atenção ao custo de se realizar algo. E nenhuma ao custo de não realizá-lo."<br />Philip Kotler</cite>
+				</div>
+			</div>
+		</div>
+		<div class="container-fluid copyright">
+            <footer>
+                <p><a href="#" title="#">&copy; 2018 Todos os direitos reservados | Noleak | Site criado por Thiago Silva</a></p>
+            </footer>
+		</div>
+		<!-- Script para o Google Analytics-->
+        <script>
+            var _gaq=[['_setAccount','xxxxxxxxxxxxxx'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+    </body>
+</html>
